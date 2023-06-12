1fca268d75e3e1e160a3a933d116bbe4fea610b8
snigbar <snigbar@outlook.com>
2023-06-12 15:14:31 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -14,9 +14,42 @@ app.use(cors(corsConfig));
 app.use(express.json())
 
 // response
-app.get('/', (req,res)=>{
-    res.send("running beta........")
-})
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9yxwfb.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    
+    app.get('/', (req,res)=>{
+        res.send("running beta........")
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.listen(port)
